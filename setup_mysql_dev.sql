9603f429f094b7aaa1e52c968249a8b0b82f54c3
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-13 21:01:42 -0700

    adding setup script that CREATES mysql database and GRANTS privileges to 2 DB's

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- creates MySQL database only if not existing
+-- and gives privileges to hbnb_dev_db on 2 DB's
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+      TO hbnb_dev@localhost
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT PRIVILEGES ON performance_schema.*
+      TO hbnb_dev@localhost
+      IDENTIFIED BY 'hbnb_dev_pwd';
