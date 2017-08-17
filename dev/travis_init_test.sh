be7fa31fe9f00126edd92b693fbd0c9e5af8f818
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-16 23:33:48 -0700

    setting up travis init script

--- dev/travis_init_test.sh
+++ dev/travis_init_test.sh
@@ -3,20 +3,21 @@
 # Checks pep8 style of all python files
 # also runs all unittests
 
-#service mysql start
+service mysql start
 
-#spawn cat setup_mysql_dev.sql | mysql -hlocalhost -uroot -p
-#expect "password:"
-#send "root"
-#interact
-#    && HBNB_MYSQL_USER=hbnb_dev \
-#		      HBNB_MYSQL_PWD=hbnb_dev_pwd \
-#		      HBNB_MYSQL_HOST=localhost \
-#		      HBNB_MYSQL_DB=hbnb_dev_db \
-#		      HBNB_TYPE_STORAGE=db \
-#		      python3 -m unittest discover -v ./tests/ \
+cat setup_mysql_dev.sql | mysql -hlocalhost -uroot
 
-pep8 . && python3 -m unittest discover -v ./tests/
+pep8 . && python3 -m unittest discover -v ./tests/ \
+    && HBNB_MYSQL_USER=hbnb_dev \
+		      HBNB_MYSQL_PWD=hbnb_dev_pwd \
+		      HBNB_MYSQL_HOST=localhost \
+		      HBNB_MYSQL_DB=hbnb_dev_db \
+		      HBNB_TYPE_STORAGE=db \
+		      python3 -m unittest discover -v ./tests/ \
+    && ./dev/w3c_validator.py \
+        $(find ./web_static -maxdepth 1 -name "*.html" -type f ! -name "4*") \
+    && ./dev/w3c_validator.py \
+	$(find ./web_static/styles -maxdepth 1 -name "*.css" -type f)
 
 # stores the return value
 ret_val=$?
