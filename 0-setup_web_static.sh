888d267e13d977b3ad627564982ee56b0677552c
KatyaKalache <katya@kalache.fr>
2017-08-22 03:57:12 +0000

    echoing with sudo tee

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,16 +5,15 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "
+echo '
 <html>
    <head>
    </head>
    <body>
      Holberton School
    </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>' | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
-sed -i "44 i location /hbnb_static/ { \n alias /data/web_static/current/; }" \
-    /etc/nginx/sites-enabled/default
+sed -i "44 i location /hbnb_static { \n alias /data/web_static/current/; }" /etc/nginx/sites-enabled/default
 service nginx restart
