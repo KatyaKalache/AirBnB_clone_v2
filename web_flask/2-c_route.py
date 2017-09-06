c20f58e8815a288a9fe5d1dda012b5d88cbcaad4
KatyaKalache <katya@kalache.fr>
2017-09-06 04:10:00 +0000

    adding import flask to 0 and 1, initial for 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,5 +8,16 @@ app = Flask(__name__)
 def hbnb():
     return "Hello HBNB!"
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_1():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_2(text):
+    text_adj = text.replace("_", " ")
+    return "C {}".format(text_adj)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
