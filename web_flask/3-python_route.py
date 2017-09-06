e1ed8dd52dfde5364b893eeed2b9a335b5a0529f
KatyaKalache <katya@kalache.fr>
2017-09-06 05:20:54 +0000

    initial

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# starts a Flask web application
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_1():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_2(text):
+    text_adj = text.replace("_", " ")
+    return "C {}".format(text_adj)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb_3(text):
+    text_u = text.replace("_", " ")
+    return ("Python {}".format(text_u))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
